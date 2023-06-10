aa6dca32cb1f6e555a51ebde5d80f1e2c02a45a5
MelianMiko <melianmiko@yandex.ru>
2023-06-11 00:49:46 +0700

    i18n: add alternative locale load format

--- i18n.js
+++ i18n.js
@@ -1,4 +1,4 @@
-const preferedLang = [
+const preferredLang = [
     hmFS.SysProGetChars("mmk_tb_lang"),
     DeviceRuntimeCore.HmUtils.getLanguage(),
     "en-US"
@@ -15,10 +15,22 @@ export function extendLocale(data) {
   }
 }
 
+export function loadLocale(code, data) {
+    // If locale isn't used, ignore
+    if(preferredLang.indexOf(code) < 0) return;
+
+    // Load all keys
+    for(let key in data) {
+        if(!strings[key]) strings[key] = {};
+        strings[key][code] = data[key];
+    }
+}
+
+
 export function t(key) {
   if(!strings[key]) return key;
 
-  for(let ln of preferedLang) {
+  for(let ln of preferredLang) {
     if(!strings[key][ln]) continue;
     return strings[key][ln];
   }
